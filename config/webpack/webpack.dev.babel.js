14b9a0867cc0410a3f5f3826af63fa683218e576
Boy Martin <boy.martin@biomerieux.com>
2019-06-14 13:00:05 +0200

    stuff

--- config/webpack/webpack.dev.babel.js
+++ config/webpack/webpack.dev.babel.js
@@ -1,40 +1,83 @@
-import webpack from 'webpack';
-import Jarvis from 'webpack-jarvis';
+import webpack from 'webpack'
+import Jarvis from 'webpack-jarvis'
 
-import paths from './paths';
+import paths from './paths'
 
 module.exports = {
-    mode: 'development',
-    output: {
-        filename: '[name].js',
-        path: paths.outputPath,
-        chunkFilename: '[name].js'
-    },
-    performance: {
-        hints: 'warning',
-        maxAssetSize: 20000000,
-        maxEntrypointSize: 8500000,
-        assetFilter: assetFilename => {
-            return (
-                assetFilename.endsWith('.css') || assetFilename.endsWith('.js')
-            );
+  mode: 'development',
+  output: {
+    filename: '[name].js',
+    path: paths.outputPath,
+    chunkFilename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/react', { 'plugins': ['@babel/plugin-proposal-class-properties'] }],
+          plugins: [
+            ['import', { 'libraryName': 'antd', 'libraryDirectory': 'es', 'style': true }]
+          ]
         }
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    devServer: {
-        contentBase: paths.outputPath,
-        compress: true,
-        hot: true,
-        historyApiFallback: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new Jarvis({
-            port: 1337
-        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: '[local]___[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'less-loader' // compiles Less to CSS
+          }
+        ]
+      }
     ]
-};
+  },
+  performance: {
+    hints: 'warning',
+    maxAssetSize: 20000000,
+    maxEntrypointSize: 8500000,
+    assetFilter: assetFilename => {
+      return (
+        assetFilename.endsWith('.css') || assetFilename.endsWith('.js')
+      )
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  devServer: {
+    contentBase: paths.outputPath,
+    compress: true,
+    hot: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new Jarvis({
+      port: 1337
+    })
+  ]
+}
